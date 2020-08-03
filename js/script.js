254bcfc4b766ca173ec21f2a3f9179fab3aeed92
lorenzo-damico <loredam@hotmail.it>
2020-08-03 16:24:55 +0200

    Aggiunto codice sicurezza tramite javascript

--- js/script.js
+++ js/script.js
@@ -7,8 +7,14 @@ var cognome = prompt("Qual è il tuo cognome?");
 // 3. Chiedo all'utente il suo colore preferito e salvo in una variabile
 var colore = prompt("Qual è il tuo colore preferito?");
 
-// 4. Creo una variabile contente la password completa
+// 4. Creo una variabile contenente la password completa
 var passwordGenerata = nome + cognome + colore;
 
-// 5. Immetto la password nel documento html
+// 5. Creo una variabile contenente un codice di sicurezza
+var codiceSicurezza = "19";
+
+// 6. Immetto la password nel documento html
 document.getElementById('password').innerHTML = passwordGenerata;
+
+// 7. Aggiungo codice di sicurezza al termine della password
+document.getElementById('code').innerHTML = codiceSicurezza;
