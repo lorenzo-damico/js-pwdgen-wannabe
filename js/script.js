2a36f05af7df455ed72cac4813f51ec9e843db3b
lorenzo-damico <loredam@hotmail.it>
2020-08-03 16:33:50 +0200

    modifica inserimento codice

--- js/script.js
+++ js/script.js
@@ -7,14 +7,11 @@ var cognome = prompt("Qual è il tuo cognome?");
 // 3. Chiedo all'utente il suo colore preferito e salvo in una variabile
 var colore = prompt("Qual è il tuo colore preferito?");
 
-// 4. Creo una variabile contenente la password completa
-var passwordGenerata = nome + cognome + colore;
-
-// 5. Creo una variabile contenente un codice di sicurezza
+// 4. Creo una variabile contenente un codice di sicurezza
 var codiceSicurezza = "19";
+
+// 5. Creo una variabile contenente la password completa
+var passwordGenerata = nome + cognome + colore + codiceSicurezza;
 
 // 6. Immetto la password nel documento html
 document.getElementById('password').innerHTML = passwordGenerata;
-
-// 7. Aggiungo codice di sicurezza al termine della password
-document.getElementById('code').innerHTML = codiceSicurezza;
